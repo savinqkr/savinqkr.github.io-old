cdb6f40ca40acf3b21af6aa7c66e105dc62aceb8
savinqkr <savinqkr@gmail.com>
2024-12-05 14:21:42 +0900

    🧪 Test Github Pages

---
+++ src/common/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+  return <header>SAVINQKR</header>;
+};
+
+export default Header;
---
+++ src/constants/path.ts
@@ -0,0 +1,17 @@
+const PATH = {
+  MAIN: "/",
+  DEVLOG: {
+    MAIN: "/devlog",
+  },
+  PROJECT: {
+    MAIN: "/project",
+  },
+  ABOUT: {
+    MAIN: "/about",
+  },
+  GITHUB: "https://github.com/savinqkr",
+  NOTION: "",
+  LINKEDIN: "https://www.linkedin.com/in/savinqkr",
+};
+
+export default PATH;
